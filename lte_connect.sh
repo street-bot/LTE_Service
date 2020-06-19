3fa9aadb86078620c444c374776441a2be2314e7
streetbot-robot <streetbot-robot@streetbot.ca>
2020-06-19 00:50:57 +0000

    First Draft of Working LTE Service

---
+++ lte_connect.sh
@@ -0,0 +1,25 @@
+#!/bin/sh
+
+# To Do -> Point this at our streetbot relay
+test_host=8.8.8.8
+
+# Get WWAN0 interface unique name
+interface=$(ls /sys/class/net | grep ww)
+
+# Continuously refresh connection
+while true; do
+    ping -I $interface -c 1 -w 2 $test_host
+
+    if [ $? -eq 0 ]; then
+        echo "Connection on $interface up, reconnect not required..."
+    else
+        echo "Connection down, reconnecting..."
+        ip link set $interface down
+	qmi-network /dev/cdc-wdm0 stop
+	qmi-network /dev/cdc-wdm0 start
+	ip link set $interface up
+	udhcpc -f -n -q -t 5 -i $interface
+    fi
+
+    sleep 5
+done
